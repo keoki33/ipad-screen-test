a41135971e4a76d8aa446a91e3d49f05e06f0c0b
George <gwong323@yahoo.com>
2019-09-16 18:33:06 +0100

    beta

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import "./App.css";
+
+class App extends Component {
+  state = {
+    x: 0,
+    y: 0,
+    ar: [
+      // { time: 1510, x: 0, y: 0 },
+      // { time: 1610, x: 10, y: 10 },
+      // { time: 1520, x: 20, y: 20 }
+    ],
+    ar2: []
+  };
+
+  handleClick = e => {
+    let now = new Date();
+    let clock = now.getHours() + ":" + now.getMinutes();
+    // this.setState({ x: e.pageX, y: e.pageY });
+    this.setState({
+      ar: [...this.state.ar, { time: clock, x: e.pageX, y: e.pageY }],
+      ar2: [
+        ...this.state.ar2,
+        <div
+          className="spot"
+          style={{ left: `${e.pageX - 10}px`, top: `${e.pageY - 10}px` }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          x
+        </div>
+      ]
+    });
+  };
+
+  displayList = () => {
+    let list = this.state.ar.map((z, i) => (
+      <li key={i}>
+        {i}. time: {z.time} x: {z.x} Y: {z.y}
+      </li>
+    ));
+    return list;
+  };
+
+  //   displaySpot = () => {
+  // let list = this.state.ar2.map((z,i) =>  )
+
+  //   }
+
+  render() {
+    return (
+      <div
+        onClick={e => {
+          this.handleClick(e);
+        }}
+        className="main"
+      >
+        {this.state.ar2}
+        <div className="list">
+          <ul>{this.displayList()}</ul>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
